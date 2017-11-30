e1b5d16aa394f4e3f992f7a49feb8a32e19ce5a3
Tiago Celestino <tcelestino@gmail.com>
2017-11-30 07:58:45 -0200

    delete files unecessary

--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.get('/', (req, res) => res.send('Server running'));
-
-router.get('/webhook', () => {});
-router.post('/webhook', () => {});
-
-module.exports = router;
