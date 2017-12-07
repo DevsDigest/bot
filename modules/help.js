1681b9e21b973e0b94092057b5524473aefed2f2
Tiago Celestino <tcelestino@gmail.com>
2017-12-07 08:48:21 -0200

    create modules with help commands

---
+++ modules/help.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const help = [
+  { title: 'show categories', 
+    subtitle: 'look all feeds categories' 
+  }, {
+    title: 'help or help me',
+    subtitle: 'list all commands allow in the use Devs Digest Bot'
+  }
+];
+
+function helpList(chat) {
+  chat.sendListTemplate(help, '', { typing: true, topElementStyle: 'compact' });
+}
+
+module.exports = (bot) => {
+  bot.hear([/help( me)?/i], (payload, chat) => {
+    helpList(chat);
+  });
+
+  bot.on('postback:PERSISTENT_MENU_HELP', (payload, chat) => {
+    console.log('foo');
+    helpList(chat);
+  });
+};
