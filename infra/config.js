fbf3cc56afdafd38e415ba674c64c101e7e1b19f
Tiago Celestino <tcelestino@gmail.com>
2017-12-11 07:34:10 -0200

    changes to file settings

--- infra/config.js
+++ infra/config.js
@@ -4,18 +4,20 @@ const joi = require('joi');
 const schema = joi.object({
   API_URI: joi.string().default('http://localhost:5000/v1'),
   FACEBOOK_VERIFY_TOKEN: joi.string(),
-  FACEBOOK_PAGE_ACCESS: joi.string()
+  FACEBOOK_PAGE_ACCESS: joi.string(),
+  FACEBOOK_APP_SECRET: joi.string()
 }).unknown().required();
 
 const { error, value: envVars } = joi.validate(process.env, schema);
 
-if (error) throw new Error(`Configuration validation error: ${error.message}`);
+if (error) throw new Error(`Settings environments error: ${error.message}`);
 
 const config = {
   api: envVars.API_URI,
   facebook: {
     token: envVars.FACEBOOK_VERIFY_TOKEN,
-    pageAccess: envVars.FACEBOOK_PAGE_ACCESS
+    pageAccess: envVars.FACEBOOK_PAGE_ACCESS,
+    appSecret: envVars.FACEBOOK_APP_SECRET
   }
 };
 
